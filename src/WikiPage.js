cba0b3cbfa97fee5851622b44203465dd8984968
junhek2525 <junhek2525@gmail.com>
2024-12-03 20:17:21 +0900

    12_03(마지막)

--- src/WikiPage.js
+++ src/WikiPage.js
@@ -1,14 +1,43 @@
-
+import "./wikipage.css";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function WikiPage(){
     const {searchQuery} = useParams();
+    const [data,setData] = useState(null);
+    const [error,setError] = useState(null);
+    useEffect(()=>{
+        const fatchData = async() =>{
+            try{
+                const response = await fetch(`/api/colors/${searchQuery}`)
+                if(response.ok)
+                {
+                    const myData = await response.json();
+                setData(myData);
+            }else{
+                const errorMessage = await response.json()
+                setError(errorMessage.message);
+            }}catch(error){
+                setError("데이터 실패");
+            }
+        };
+        fatchData();
+    }, [searchQuery])
     return(
         <div className="container">
         <h1>{searchQuery}</h1>
-        
-        <div className="article-container"></div>
-        <h2>내용이 읎어</h2>
+        <div className="article-container">
+        {error ? (
+            <p>{error}</p>
+        ) : data ? (
+            <div>
+            <p>색상 공간: {data.색상공간}</p>
+            <p>설명: {data.설명}</p>
+            </div>
+        ) : (
+            <p>데이터를 로드 중입니다.</p>
+        )}
+        </div>
         </div>
     )
 }
